0fcf601e131aac9234e9a6142ee0e0f8a453955c
IhorKozloff <ik.eyeshield21@gmail.com>
2022-08-23 21:00:43 +0300

    ...

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const { PORT = 3001, MONGO_URL } = process.env;
 
-const { PORT = 3001 } = process.env;
+console.log(MONGO_URL)
+mongoose
+.connect(MONGO_URL)
+    .then(app.listen(PORT))
+    .then(() => {
+        console.log(`Database connected`)
+        console.log(`Server run on port ${PORT}`)
+    })
+    .catch(error => () => {
+        console.log(error)
+        process.exit(1)
+    });
 
-
-
-
-    app.listen(PORT)
+    
