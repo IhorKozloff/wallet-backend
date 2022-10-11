8935c3ab0d222283cf5e38993326ca6e215ed19e
IhorKozloff <ik.eyeshield21@gmail.com>
2022-10-11 17:07:19 +0300

    populate owner in addTransaction

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -4,7 +4,7 @@ const addTransactions = async (req, res) => {
 
     const { _id: owner} = req.user;
 
-    const transaction = await Transaction.create({...req.body, owner});
+    const transaction = await Transaction.create({...req.body, owner}).populate("owner", "name email");
 
     res.json({
         status: "success",
