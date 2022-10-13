691b40524ad91cd99904d7faf4fb5a293e938f4c
IhorKozloff <ik.eyeshield21@gmail.com>
2022-10-13 19:37:13 +0300

    Update addTransaction.js

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -4,14 +4,12 @@ const addTransactions = async (req, res) => {
 
     const { _id: owner} = req.user;
 
-    const transaction = await Transaction.create({...req.body, owner}).populate("owner", "name email");
+    const transaction = await Transaction.create({...req.body, owner});
 
     res.json({
         status: "success",
         code: 200,
-        data: {
-            result: transaction
-        }
+        result: transaction
     });
 }
 
