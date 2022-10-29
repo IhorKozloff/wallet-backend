d3d6c4b98851b40fdbd8a3b0cb8e432e18364cb2
IhorKozloff <ik.eyeshield21@gmail.com>
2022-10-29 20:59:21 +0300

    Update addDragonReviewerUserData.js

--- controllers/dragonReviewerUserData/addDragonReviewerUserData.js
+++ controllers/dragonReviewerUserData/addDragonReviewerUserData.js
@@ -1,13 +1,24 @@
 const { DragonReviewerUserData }  = require('../../models/dragonReviewerUserData');
 
 const addUserData = async (req, res) => {
+    let result = null;
+    const { email } = req.body;
+    console.log(email)
+    const userData = await DragonReviewerUserData.findOne({email: email});
 
-    const transaction = await DragonReviewerUserData.create(req.body);
+    if (!userData) {
+        result = await DragonReviewerUserData.create(req.body);
+    } else {
+        // await DragonReviewerUserData.findOneAndRemove({email: email});
+        // result = await DragonReviewerUserData.create(req.body);
+        result = await DragonReviewerUserData.findOneAndUpdate({email: email}, req.body, {new: true});
+    }
+    
 
     res.json({
         status: "success",
         code: 200,
-        result: transaction
+        result: result
     });
 }
 
